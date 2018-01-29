6cb18fb590868a099987b3927c20c4564b21a70d
cai-eugene <cai_eugene@bah.com>
2018-01-29 11:52:25 -0500

    modified index.html version

--- Juvvo/WebContent/resources/script.js
+++ Juvvo/WebContent/resources/script.js
@@ -1,40 +1,3 @@
-
-function mySubmit(button){
-  //alert("ok");
-  var prov = document.getElementById("provocative");
-  var selected = document.getElementById("selected");
-  var out1 = document.getElementById("output1");
-
-  if(document.getElementById("button").value == "Submit"){
-    fade(prov);
-    fade(selected);
-    setTimeout(function(){
-      prov.style.display = "none";
-      selected.style.display = "none";
-      unfade(out1);
-      out1.classList.add("output");
-      document.getElementById("button").value = "Reset";
-    },800);
-  }else if(document.getElementById("button").value == "Reset"){
-    fade(out1);
-    setTimeout(function(){
-      out1.style.display = "none";
-      unfade(prov);
-      unfade(selected);
-      $("area").mapster("set", false);
-      document.getElementById("selectText").innerHTML = "";
-
-      var ul = document.getElementById("listQ");
-      var items = ul.getElementsByTagName("li");
-      for (var i = 0; i < items.length; ++i) {
-        items[i].classList.add("provQ");
-      }
-
-      document.getElementById("button").value = "Submit";
-    }, 800);
-  }
-}
-
 function fade(element) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
@@ -50,6 +13,9 @@ function fade(element) {
 
 function unfade(element) {
     var op = 0.1;  // initial opacity
+    element.style.opacity = op;
+    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+
     element.style.display = 'inline-block';
     var timer = setInterval(function () {
         if (op >= 1){
@@ -61,48 +27,191 @@ function unfade(element) {
     }, 10);
 }
 
+function showPara(obj){
+  if(obj.value === "Yes"){
+    document.getElementById("parasthesia").style.display = "inline-block";
+    document.getElementById("sensation").required = true;
+  }else{
+    document.getElementById("parasthesia").style.display = "none";
+    document.getElementById("sensation").required = false;
+  }
+}
+
+function validationResult(){
+  if(document.getElementById("firstName").value === ""){
+    document.getElementById("firstName").style.borderColor = "red";
+    document.getElementById("firstNameLabel").style.color = "red";
+  }else{
+    document.getElementById("firstName").style.borderColor = "#0799CC";
+    document.getElementById("firstNameLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("lastName").value === ""){
+    document.getElementById("lastName").style.borderColor = "red";
+    document.getElementById("lastNameLabel").style.color = "red";
+  }else{
+    document.getElementById("lastName").style.borderColor = "#0799CC";
+    document.getElementById("lastNameLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("age").value === ""){
+    document.getElementById("age").style.borderColor = "red";
+    document.getElementById("ageLabel").style.color = "red";
+  }else{
+    document.getElementById("age").style.borderColor = "#0799CC";
+    document.getElementById("ageLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("gender").value === ""){
+    document.getElementById("gender").style.borderColor = "red";
+    document.getElementById("genderLabel").style.color = "red";
+  }else{
+    document.getElementById("gender").style.borderColor = "#0799CC";
+    document.getElementById("genderLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("ethnicity").value === ""){
+    document.getElementById("ethnicity").style.borderColor = "red";
+    document.getElementById("ethnicityLabel").style.color = "red";
+  }else{
+    document.getElementById("ethnicity").style.borderColor = "#0799CC";
+    document.getElementById("ethnicityLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("typeofInjury").value === ""){
+    document.getElementById("typeofInjury").style.borderColor = "red";
+    document.getElementById("typeofInjuryLabel").style.color = "red";
+  }else{
+    document.getElementById("typeofInjury").style.borderColor = "#0799CC";
+    document.getElementById("typeofInjuryLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("presenceofHeadaches").value === ""){
+    document.getElementById("presenceofHeadaches").style.borderColor = "red";
+    document.getElementById("presenceofHeadachesLabel").style.color = "red";
+  }else{
+    document.getElementById("presenceofHeadaches").style.borderColor = "#0799CC";
+    document.getElementById("presenceofHeadachesLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("duration").value === ""){
+    document.getElementById("duration").style.borderColor = "red";
+    document.getElementById("durationLabel").style.color = "red";
+  }else{
+    document.getElementById("duration").style.borderColor = "#0799CC";
+    document.getElementById("durationLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("shoulderPain").value === ""){
+    document.getElementById("shoulderPain").style.borderColor = "red";
+    document.getElementById("shoulderPainLabel").style.color = "red";
+  }else{
+    document.getElementById("shoulderPain").style.borderColor = "#0799CC";
+    document.getElementById("shoulderPainLabel").style.color = "#0799CC";
+  }
+
+  if(document.getElementById("radiculopathy").value === ""){
+    document.getElementById("radiculopathy").style.borderColor = "red";
+    document.getElementById("radiculopathyLabel").style.color = "red";
+  }else{
+    document.getElementById("radiculopathy").style.borderColor = "#0799CC";
+    document.getElementById("radiculopathyLabel").style.color = "#0799CC";
+    if(document.getElementById("radiculopathy").value === "Yes"){
+      if(document.getElementById("sensation").value === ""){
+        document.getElementById("sensation").style.borderColor = "red";
+        document.getElementById("sensationLabel").style.color = "red";
+      }else{
+        document.getElementById("sensation").style.borderColor = "#0799CC";
+        document.getElementById("sensationLabel").style.color = "#0799CC";
+      } 
+    }else{
+      document.getElementById("sensation").style.borderColor = "#0799CC";
+      document.getElementById("sensationLabel").style.color = "#0799CC";
+    }
+  }
+
+return true;
+/*
+  if($('#myForm')[0].checkValidity()){
+    return true;
+  }else{
+    document.getElementById("validation").style.display = "block";
+    return false; 
+  }*/
+}
+
+function showOutput() {
+  if(document.getElementById("submitButt").value==="Submit"){
+    if(validationResult()){
+      var prov = document.getElementById("general");
+      var out1 = document.getElementById("output1");
+      
+      fade(prov);
+      setTimeout(function () {
+          prov.style.display = "none";
+          unfade(out1);
+          out1.classList.add("output");
+          document.getElementById("submitButt").value = "Reset";
+      }, 800);
+    }
+  }else{
+    location.reload();
+  }
+}
+
 $(document).ready(function () {
-               // a cross reference of area names to text to be shown for each area
+  var xref = { 
+     l1: {id: "l1", select: false, message: "You have selected L1"}
+  };
 
-             var xref = { 
-                 l1: {id: "l1", select: false, message: "You have selected L1"},
-                 l2: {id: "l2", select: false, message: "You have selected L2"},
-                 l3: {id: "l3", select: false, message: "You have selected L3"},
-                 l4: {id: "l4", select: false, message: "You have selected L4"},
-                 l5: {id: "l5", select: false, message: "You have selected L5"},      
-                 s1: {id: "s1", select: false, message: "You have selected S1"},
-                 s2: {id: "s2", select: false, message: "You have selected S2"},
-                 s3: {id: "s3", select: false, message: "You have selected S3"},
-                 s4: {id: "s4", select: false, message: "You have selected S4"},
-                 s5: {id: "s5", select: false, message: "You have selected S5"}};
+    var image = $('img');
+    image.mapster(
+    {
+         fillOpacity: 0.4,
+         fillColor: "d42e16",
+         strokeColor: "04B2E0",
+         strokeOpacity: 0.8,
+         strokeWidth: 2,
+         stroke: true,
+         isSelectable: true,
+         isDeselectable: true,
+         singleSelect: false,
+         mapKey: 'name',
+         listKey: 'name',
+         onClick: function(e){
+          xref[e.key]["select"] = !xref[e.key]["select"];
+            /*if(xref[e.key]["select"]){
+              var oldHTML = document.getElementById("selectText").innerHTML;
+              document.getElementById("selectText").innerHTML = oldHTML + '<li style="margin: 5px" id="' + xref[e.key]["id"] + '">' + xref[e.key]["message"] + '</li>';
+              document.getElementById(xref[e.key]["id"] + "Q").classList.remove("provQ");
+            }else{
+              document.getElementById(xref[e.key]["id"]).remove();
+              document.getElementById(xref[e.key]["id"] + "Q").classList.add("provQ");
+            } */
 
-                var image = $('img');
-                image.mapster(
-                {
-                     fillOpacity: 0.4,
-                     fillColor: "d42e16",
-                     strokeColor: "04B2E0",
-                     strokeOpacity: 0.8,
-                     strokeWidth: 2,
-                     stroke: true,
-                     isSelectable: true,
-                     isDeselectable: true,
-                     singleSelect: false,
-                     mapKey: 'name',
-                     listKey: 'name',
-                     onClick: function(e){
-                      xref[e.key]["select"] = !xref[e.key]["select"];
-                        if(xref[e.key]["select"]){
-                          var oldHTML = document.getElementById("selectText").innerHTML;
-                          document.getElementById("selectText").innerHTML = oldHTML + '<li style="margin: 5px" id="' + xref[e.key]["id"] + '">' + xref[e.key]["message"] + '</li>';
-                          document.getElementById(xref[e.key]["id"] + "Q").classList.remove("provQ");
-                        }else{
-                          document.getElementById(xref[e.key]["id"]).remove();
-                          document.getElementById(xref[e.key]["id"] + "Q").classList.add("provQ");
-                        } 
-                      }
+            if(xref[e.key]["select"]){
+              //if(xref[e.key]["id"] == "shoulder"){
+                //var oldHTML = document.getElementById("selectText").innerHTML;
+                //document.getElementById("selectText").innerHTML = oldHTML + '<li style="margin: 5px" id="' + xref[e.key]["id"] + '">' + xref[e.key]["message"] + '</li>';
+               // document.getElementById(xref[e.key]["id"] + "Q").classList.remove("provQ");
+              //}
+                if(xref[e.key]["id"] === "l1"){
+                  var gen = document.getElementById("general");
+                  var butt = document.getElementById("submitForm");
+                  unfade(gen);
+                  unfade(butt);
+                }
+            }else{
+              if(xref[e.key]["id"] === "l1"){
+                  document.getElementById("general").style.display = "none";
+                  document.getElementById("submitButt").style.display = "none";
+                }
+              //document.getElementById(xref[e.key]["id"]).remove();
+             // document.getElementById(xref[e.key]["id"] + "Q").classList.add("provQ");
+            }
+          }
 
 
-                     
-               });
-            });
+         
+   });
+});
